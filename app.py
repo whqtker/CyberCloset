24de535d160f680a2ecc843e100b0fdbd7683b31
whqtker <chosh2001@naver.com>
2024-06-25 22:03:01 +0900

    image to DB

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+
+# MongoDB 클라이언트 연결
+client = MongoClient('mongodb://localhost:27017')
+db = client['project']
+collection = db['my']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/save_data', methods=['POST'])
+def save_data():
+    type = request.form['type']
+    detail = request.form['detail']
+    color = request.form['color']
+    image = request.files['image']
+
+    # 이미지 파일 저장
+    image_filename = image.filename
+    image.save(os.path.join('static', image_filename))
+
+    data = {
+        'type': type,
+        'detail': detail,
+        'color': color,
+        'image': image_filename
+    }
+
+    # MongoDB에 데이터 저장
+    collection.insert_one(data)
+
+    return jsonify({'message': '저장되었습니다.'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
