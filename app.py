5d8a1240d2dc420e680e1e6e91c0220f6a02b398
whqtker <chosh2001@naver.com>
2024-06-26 23:58:06 +0900

    코디 등록 추가(이미지 부분 수정필요)

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 # MongoDB 클라이언트 연결
 client = MongoClient('mongodb://localhost:27017')
 db = client['project']
-collection = db['my']
+my = db['my']
+outfit = db['outfit']
 
 @app.route('/')
 def index():
@@ -17,31 +18,43 @@ def index():
 @app.route('/brands')
 def get_brands():
     # MongoDB에서 distinct한 브랜드 목록 가져오기
-    brands = list(collection.distinct('brand'))
+    brands = list(my.distinct('brand'))
     return jsonify(brands)
 
-@app.route('/insert')
-def insert():
-    return render_template('insert.html')
+@app.route('/insert_my')
+def insert_my():
+    return render_template('insert_my.html')
 
-@app.route('/show')
-def show():
+@app.route('/show_my')
+def show_my():
     # MongoDB에서 데이터 조회
-    data = list(collection.find())
+    data = list(my.find())
 
     # 조회한 데이터를 템플릿에 전달
-    return render_template('show.html', data=data)
+    return render_template('show_my.html', data=data)
+
+@app.route('/insert_outfit')
+def insert_outfit():
+    return render_template('insert_outfit.html')
+
+@app.route('/show_outfit')
+def show_outfit():
+    # MongoDB에서 데이터 조회
+    data = list(outfit.find())
+
+    # 조회한 데이터를 템플릿에 전달
+    return render_template('show_outfit.html', data=data)
 
 @app.route('/delete/<string:item_id>', methods=['DELETE'])
 def delete_item(item_id):
     try:
-        collection.delete_one({'_id': ObjectId(item_id)})
+        my.delete_one({'_id': ObjectId(item_id)})
         return jsonify({'message': '항목이 삭제되었습니다.'}), 200
     except Exception as e:
         return jsonify({'error': '삭제 중 오류가 발생했습니다.'}), 500
 
-@app.route('/save_data', methods=['POST'])
-def save_data():
+@app.route('/save_data_my', methods=['POST'])
+def save_data_my():
     type = request.form['type']
     detail = request.form['detail']
     brand = request.form['brand']
@@ -64,9 +77,42 @@ def save_data():
         data['image'] = 'default.jpg'  # 기본 이미지 파일명
 
     # MongoDB에 데이터 저장
-    collection.insert_one(data)
+    my.insert_one(data)
 
     return jsonify({'message': '저장되었습니다.'}), 200
+
+@app.route('/save_data_outfit', methods=['POST'])
+def save_data_outfit():
+    outfit_data = []
+    for i in range(int(request.form['outfit_count'])):
+        type = request.form[f'type-{i}']
+        detail = request.form[f'detail-{i}']
+        brand = request.form[f'brand-{i}']
+        color = request.form[f'color-{i}']
+        image = request.files.get(f'image-{i}', None)
+
+        outfit_item = {
+            'type': type,
+            'detail': detail,
+            'brand': brand,
+            'color': color
+        }
+
+        if image:
+            # 이미지 파일 저장
+            image_filename = image.filename
+            image.save(os.path.join('static', image_filename))
+            outfit_item['image'] = image_filename
+        else:
+            outfit_item['image'] = 'default.jpg'  # 기본 이미지 파일명
+
+        outfit_data.append(outfit_item)
+
+    # MongoDB에 데이터 저장
+    outfit.insert_one({'outfit': outfit_data})
+
+    return jsonify({'message': '저장되었습니다.'}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
